ef69b8afb667e16bbb3b6c3672986bc188a445b1
JeanMichaelC <JeanMichaelC@randomemail.com>
2022-10-02 18:54:18 +0200

    :memo: .repeat()
    Just a string mehtod

---
+++ BuiltIn-Objects/String/Methods/String.prototype.repeat().js
@@ -0,0 +1,7 @@
+// The repeat() method constructs and returns a new string which contains the specified number of copies of the string on which it was called, concatenated together
+
+const chorus = 'Because I\'m happy. ';
+
+console.log(`Chorus lyrics for "Happy": ${chorus.repeat(3)}`);
+
+// expected output: "Chorus lyrics for "Happy": Because I'm happy. Because I'm happy. Because I'm happy. "
